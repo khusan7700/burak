f51ff7011effb3e986490508d639c79b9a67bfc8
Khusan <khusan7700@gmail.com>
2024-02-23 01:46:44 +0900

    architectural & design pattern

--- src/server.ts
+++ src/server.ts
@@ -7,3 +7,22 @@
 
 // const person: string = "Usman";
 // const count: number = 100;
+//---------------------------------------
+
+// Architectural pattern: MVC, Dependency Injection, MVP
+
+/* bizning backendimizning suyadgi 
+yani bizni backendimizdagi malumotlar oqimini tartibga
+soladigon bir vosita yani arxitukturasi hisoblanadi.
+--insonni suyagi--
+*/
+
+// MVC = model view controller( juda ham muhum petterin asosal backendda ishlatiladi)
+
+// Design pattern: Middlewate, Decotar
+
+/* butun bir backend tizimini tartibga soluvgi vositachi
+sifatida emas balki malum bir bolaklarini strukturasini
+ yechada hizmat ko'rsatadigon petternlar hisoblanadi. 
+ malum bir vazifani bajaradigon qism
+ --qol suyagi--*/
