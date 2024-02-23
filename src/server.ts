43fdc372af254adc043ae0c713970ac2e692a42a
Khusan <khusan7700@gmail.com>
2024-02-24 04:37:23 +0900

    feat: connect mongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -34,8 +34,18 @@ sifatida emas balki malum bir bolaklarini strukturasini
 import dotenv from "dotenv";
 dotenv.config();
 
-// console.log("PORT:", process.env.PORT);
+console.log("PORT:", process.env.PORT);
 
-// console.log("MONGO_URL:", process.env.MONGO_URL);
+console.log("MONGO_URL:", process.env.MONGO_URL);
 
 //cluster => database => collection => document (mongodb)
+
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("mongoDB connection secceed");
+    const PORT = process.env.PORT ?? 3003;
+  })
+  .catch((err) => console.log("ERROR on connection MongoDB", err));
