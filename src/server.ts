38c75a639a9ed3c89aa9fc9d184c32f6d284f039
Khusan <khusan7700@gmail.com>
2024-02-26 00:19:01 +0900

    feat: express integration

--- src/server.ts
+++ src/server.ts
@@ -34,18 +34,21 @@ sifatida emas balki malum bir bolaklarini strukturasini
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log("PORT:", process.env.PORT);
+// console.log("PORT:", process.env.PORT);
 
-console.log("MONGO_URL:", process.env.MONGO_URL);
+// console.log("MONGO_URL:", process.env.MONGO_URL);
 
 //cluster => database => collection => document (mongodb)
-
 import mongoose from "mongoose";
+import app from "./app";
 
 mongoose
   .connect(process.env.MONGO_URL as string, {})
   .then((data) => {
     console.log("mongoDB connection secceed");
     const PORT = process.env.PORT ?? 3003;
+    app.listen(PORT, function () {
+      console.log(` The server is running successfully on pory: ${PORT}`);
+    });
   })
   .catch((err) => console.log("ERROR on connection MongoDB", err));
--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,14 @@
 console.log("Train.ts ishga tushdi!");
+
+/*
+Shunday function tuzing, u string qabul qilib teskari
+ qilib return qilsin
+MASALAN: gerReverse("hello") return qiladi "olleh"
+
+*/
+
+function gerReverse(str: string): string {
+  return str.split("").reverse().join("");
+}
+
+console.log(gerReverse("hello"));
