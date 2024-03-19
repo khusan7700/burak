4569fb6d5210b1f260e6978449b36b8851b11192
Khusan <khusan7700@gmail.com>
2024-03-20 03:26:42 +0900

    feat: sigbup process frontend validation

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -49,6 +49,7 @@ restaurantController.processSignup = async (
 ) => {
   try {
     console.log("processSignup");
+    console.log("req.body:", req.body);
     const file = req.file; //request filedan yuklab olyapmiz
     if (!file)
       throw new Errors(HttpCode.BAD_REQUEST, Message.SOMETHING_WENT_WRONG);
