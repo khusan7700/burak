62183ac1637b2cabf7d7a1e699eb9a2a35efad89
Khusan <khusan7700@gmail.com>
2024-03-22 00:26:46 +0900

    feat: create new product frontend logic develop

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -17,7 +17,6 @@ productController.getAllProducts = async (req: Request, res: Response) => {
     console.log("getAllProducts");
     const data = await productService.getAllProducts();
 
-    //-------------------------------------------
     res.render("products", { products: data });
   } catch (err) {
     console.log("Error, getAllProducts:", err);
@@ -33,6 +32,7 @@ productController.createNewProduct = async (
 ) => {
   try {
     console.log("createNewProduct");
+    console.log("req.body:", req.body);
     if (!req.files?.length)
       throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
 
@@ -44,14 +44,14 @@ productController.createNewProduct = async (
     await productService.createNewProduct(data);
 
     res.send(
-      `<script> alert("Sucsessful creation!"); window.location.replace('admin/product/all') </script>`
+      `<script> alert("Sucsessful creation!"); window.location.replace('/admin/product/all') </script>`
     );
   } catch (err) {
     console.log("Error, createNewProduct:", err);
     const message =
       err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
     res.send(
-      `<script> alert("${message}"); window.location.replace('admin/product/all') </script>`
+      `<script> alert("${message}"); window.location.replace('/admin/product/all') </script>`
     );
   }
 };
